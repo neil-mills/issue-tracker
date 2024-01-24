2cc375fabb410e46157b4d4cd5e8ca2184855c27
Neil Mills <mail@neil-mills.co.uk>
2024-01-24 12:35:14 +0000

    Setup Radix UI

---
+++ app/api/issues/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from './schema'
+import { prisma } from "@/prisma/client";
+
+export async function GET(request: NextRequest) {
+	const issues = await prisma.issue.findMany()
+	return NextResponse.json(issues)
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json()
+  const validation = schema.safeParse(body)
+  if(!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 })
+  }
+  const { title, description } = body
+  const issue = await prisma.issue.create({
+    data: { title, description }
+  })
+  return NextResponse.json(issue, { status: 201 })
+
+}
