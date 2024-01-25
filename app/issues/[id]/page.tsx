a3770235ae67d476c5e650f73e3384fa692724bd
Neil Mills <mail@neil-mills.co.uk>
2024-01-25 16:19:43 +0000

    Add edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,9 @@
 import StatusBadge from "@/app/components/StatusBadge"
-import { Card, Flex, Heading, Text } from "@radix-ui/themes"
+import { Grid, Card, Flex, Heading, Text, Box, Button } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
 import ReactMarkdown from "react-markdown"
+import { Pencil2Icon } from '@radix-ui/react-icons'
+import Link from "next/link"
 
 interface Props {
   params: { id: string }
@@ -14,7 +16,8 @@ const IssueDetailPage = async ({params}:Props ) => {
   })
   if(!issue) notFound()
   return (
-    <div>
+    <Grid columns={{initial: "1", md:"2" }} gap="5">
+      <Box>
       <Heading as="h2">{issue.title}</Heading>
       <Flex gap="2" className="my-2">
       <StatusBadge status={issue.status} />
@@ -23,7 +26,11 @@ const IssueDetailPage = async ({params}:Props ) => {
       <Card className="prose" mt="4">
       <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
-    </div>
+      </Box>
+      <Box>
+        <Button><Pencil2Icon /><Link href={`/issues/${issue.id}/edit`}>Edit issue</Link></Button>
+      </Box>
+    </Grid>
   )
 }
 
