2b9985fc96cb85a1158f28dede47f97eb14e5bc5
Neil Mills <mail@neil-mills.co.uk>
2024-01-25 16:34:09 +0000

    Refactor: apply the SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import { StatusBadge } from '@/app/components'
+import { Heading, Flex, Card, Text } from '@radix-ui/themes'
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Issue } from '@prisma/client'
+
+const IssueDetails = ({issue}:{issue:Issue}) => {
+  return (
+    <>
+  <Heading as="h2">{issue.title}</Heading>
+      <Flex gap="2" className="my-2">
+      <StatusBadge status={issue.status} />
+      <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose" mt="4">
+      <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+  </>
+    )}
+
+export default IssueDetails
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,9 @@
 import StatusBadge from "@/app/components/StatusBadge"
-import { Grid, Card, Flex, Heading, Text, Box, Button } from "@radix-ui/themes"
+import { Box, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
 import ReactMarkdown from "react-markdown"
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from "next/link"
+import EditIssueButton from "./EditIssueButton"
+import IssueDetails from "./IssueDetails"
 
 interface Props {
   params: { id: string }
@@ -18,17 +18,10 @@ const IssueDetailPage = async ({params}:Props ) => {
   return (
     <Grid columns={{initial: "1", md:"2" }} gap="5">
       <Box>
-      <Heading as="h2">{issue.title}</Heading>
-      <Flex gap="2" className="my-2">
-      <StatusBadge status={issue.status} />
-      <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-      <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
+    <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button><Pencil2Icon /><Link href={`/issues/${issue.id}/edit`}>Edit issue</Link></Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   )
