26896617b35f3be02088c85ddb37c8cb3234d22a
Neil Mills <mail@neil-mills.co.uk>
2024-01-26 17:13:43 +0000

    Change layout of navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import classNames from 'classnames'
 import { useSession } from 'next-auth/react'
-import { Box } from '@radix-ui/themes'
+import { Box, Container, Flex } from '@radix-ui/themes'
 
 interface LinkType {
   label: string
@@ -30,15 +30,21 @@ const NavBar = () => {
     )
   }
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/"><AiFillBug /></Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => NavItem(link))}
-      </ul>
-      <Box>
-        {status === "authenticated" && <Link href="/api/auth/signout">Sign out</Link>}
-        {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
-      </Box>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+      <Flex justify={"between"}>
+        <Flex align="center" gap="3">
+        <Link href="/"><AiFillBug /></Link>
+        <ul className="flex space-x-6">
+          {links.map((link) => NavItem(link))}
+        </ul>
+        </Flex>
+        <Box>
+          {status === "authenticated" && <Link href="/api/auth/signout">Sign out</Link>}
+          {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
+        </Box>
+      </Flex>
+      </Container>
     </nav>
   )
 }
