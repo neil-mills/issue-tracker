4a9fca0a8314e20c20610a04cd5687464666dca4
Neil Mills <mail@neil-mills.co.uk>
2024-01-26 09:44:04 +0000

    Build the edit issue page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,5 @@
-import StatusBadge from "@/app/components/StatusBadge"
-import { Box, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
+import { Box, Grid } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
-import ReactMarkdown from "react-markdown"
 import EditIssueButton from "./EditIssueButton"
 import IssueDetails from "./IssueDetails"
 
